7a0320f52130e0469adb21931c014bd74e3fb46a
rmarchana2004-jpg <rmarchana2004@gmail.com>
2026-09-19 17:46:31 +0530

    Connect frontend to live backend

--- frontend/script.js
+++ frontend/script.js
@@ -55,7 +55,7 @@ async function sendMessage() {
     try {
 
         const response = await fetch(
-            "http://127.0.0.1:8000/chat",
+            "https://ecomind-ai-1x90.onrender.com/chat",
             {
                 method: "POST",
 
